d9e48d4b68394fe732ac9deed1bf266df0dec644
cafaddict <cafaddict93@gmail.com>
2024-10-09 18:23:46 +0900

    Setting up entity in opengl

--- Engine/Rendering/OpenGL/include/OpenGLRenderer.hpp
+++ Engine/Rendering/OpenGL/include/OpenGLRenderer.hpp
@@ -2,8 +2,15 @@
 #include "EntityManager.hpp"
 #include "Rendering.hpp"
 #include "Log.hpp"
+#include "Camera.hpp"
 
 namespace Engine {
+struct Vertex {
+    glm::vec3 pos;
+    glm::vec3 normal;
+    glm::vec4 color;
+    glm::vec2 texCoord;
+};
 class OpenGLRenderer : public Renderer {
     public:
     static OpenGLRenderer *Create(GLFWwindow *window);
@@ -21,6 +28,7 @@ class OpenGLRenderer : public Renderer {
     virtual void SetEntityUpdate(bool update) override;
     virtual void WaitIdle() override;
     void SetEntityManager(std::shared_ptr<EntityManager> entitymanager) { m_EntityManager = entitymanager; }
+    void createEntityResources();
 
     private:
     std::shared_ptr<EntityManager> m_EntityManager;
@@ -29,5 +37,6 @@ class OpenGLRenderer : public Renderer {
     bool m_Resized = false;
     bool m_EntityUpdate = false;
     std::vector<GLuint> m_VBOs;
+    std::unordered_map<std::shared_ptr<Entity>, GLuint> m_EntityVAOs;
 };
 } // namespace Engine
--- Engine/Rendering/OpenGL/src/OpenGLRenderer.cpp
+++ Engine/Rendering/OpenGL/src/OpenGLRenderer.cpp
@@ -1,5 +1,10 @@
 #include "OpenGLRenderer.hpp"
 #include "Log.hpp"
+#include "ShaderComponent.hpp"
+#include "TextureComponent.hpp"
+#include "ShaderComponent.hpp"
+#include "ModelComponent.hpp"
+#include "TransformComponent.hpp"
 namespace Engine {
 // Renderer *Renderer::Create(GLFWwindow *window) {
 //     ENGINE_INFO("Vulkan Renderer Creation");
@@ -44,5 +49,47 @@ void OpenGLRenderer::SetWindowMinimized(bool minimized) { m_Minimizied = minimiz
 bool OpenGLRenderer::GetEntityUpdate() { return m_EntityUpdate; }
 void OpenGLRenderer::SetEntityUpdate(bool update) { m_EntityUpdate = update; }
 void OpenGLRenderer::WaitIdle() {}
+
+void OpenGLRenderer::createEntityResources() {
+    uint32_t currentVertexOffset = 0;
+    uint32_t currentIndexOffset = 0;
+
+    auto entities = m_EntityManager->GetAllEntities();
+    std::vector<Vertex> vertices;
+    std::vector<uint32_t> indices;
+    std::vector<glm::mat4> transformations;
+    std::unordered_map<std::shared_ptr<Entity>, size_t> vertexOffsets;
+    std::unordered_map<std::shared_ptr<Entity>, size_t> indexOffsets;
+
+    for (auto &entity : entities) {
+        auto model_data = entity->GetComponent<ModelComponent>()->GetModelData();
+
+        auto texture_data = entity->GetComponent<TextureComponent>()->GetTextureData();
+
+        auto vertex_shader_data = entity->GetComponent<ShaderComponent>()->GetVertexShader();
+
+        auto fragment_shader_data = entity->GetComponent<ShaderComponent>()->GetFragmentShader();
+
+        auto compute_shader_data = entity->GetComponent<ShaderComponent>()->GetComputeShader();
+
+        auto transform = entity->GetComponent<TransformComponent>()->GetTransformMatrix();
+
+        size_t vertexCount = model_data->positions.size();
+        size_t indexCount = model_data->indices.size();
+
+        ENGINE_TRACE("model_data->positions.size() = {0}", model_data->positions.size());
+        ENGINE_TRACE("model_data->normals.size() = {0}", model_data->normals.size());
+        ENGINE_TRACE("model_data->uvs.size() = {0}", model_data->uvs.size());
+
+        for (size_t i = 0; i < vertexCount; i++) {
+            Vertex vertex;
+            vertex.pos = model_data->positions[i];
+            vertex.normal = model_data->normals.size() > i ? model_data->normals[i] : glm::vec3(0.0f, 0.0f, 1.0f);
+            vertex.texCoord = model_data->uvs.size() > i ? model_data->uvs[i] : glm::vec2(0.0f);
+            vertex.color = {1.0f, 1.0f, 1.0f, 1.0f};
+            vertices.push_back(vertex);
+        }
+    }
+}
 
 } // namespace Engine
